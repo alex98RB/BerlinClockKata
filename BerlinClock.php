a8e914b207771979db17c4d014016457f00a20ad
Jeremy Barras <jeremy.barras@student.vinci.be>
2020-11-02 18:49:49 +0100

    add - function minutes_per_05 and CountDivisblePer5

--- BerlinClock.php
+++ BerlinClock.php
@@ -17,7 +17,30 @@ class BerlinClock{
 
     }
 
+    public function minutes_per_05(String $string){
+        $count = $this->CountDivisiblePer5($string);
+        if($count == 0) return "OOOOOOOOOOO";
+        if($count == 1) return "OOOOOOOOOOI";
+        if($count == 2) return "OOOOOOOOOII";
+        if($count == 3) return "OOOOOOOOIII";
+        if($count == 4) return "OOOOOOOIIII";
+        if($count == 5) return "OOOOOOIIIII";
+        if($count == 6) return "OOOOOIIIIII";
+        if($count == 7) return "OOOOIIIIIII";
+        if($count == 8) return "OOOIIIIIIII";
+        if($count == 9) return "OOIIIIIIIII";
+        if($count == 10) return "OIIIIIIIII";
+        if($count == 11) return "IIIIIIIIII";
 
+    }
 
+    public function CountDivisiblePer5($int): string{
+        $count = 0;
+        while($int >= 5){
+            $count++;
+            $int = $int-5;
+        }
+        return $count;
+    }
 
 }
