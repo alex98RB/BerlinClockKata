5612654cc612f6f90f8a1512e1a58afbd7eebf7f
Jeremy Barras <jeremy.barras@student.vinci.be>
2020-11-06 15:52:05 +0100

    add - function getHours

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -41,121 +41,121 @@ class BerlinClockTest extends TestCase
     }
 
     public function test_convert_given01Minutes_shouldReturn01Light(){
-        $actual = $this->berlinClock->minutes('01');
+        $actual = $this->getMinutes('01');
 
         $this->assertEquals("OOOI", $actual);
     }
 
     public function test_convert_given02Minutes_shouldReturn02Light(){
-        $actual = $this->berlinClock->minutes('02');
+        $actual = $this->getMinutes('02');
 
         $this->assertEquals("OOII", $actual);
     }
 
     public function test_convert_given03Minutes_shouldReturn03Light(){
-        $actual = $this->berlinClock->minutes('03');
+        $actual = $this->getMinutes('03');
 
         $this->assertEquals("OIII", $actual);
     }
 
     public function test_convert_given04Minutes_shouldReturn04Light(){
-        $actual = $this->berlinClock->minutes('04');
+        $actual = $this->getMinutes('04');
 
         $this->assertEquals("IIII", $actual);
     }
 
     public function test_convert_given05Minutes_shouldReturn01_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('05');
+        $actual = $this->getMinutes_per_05('05');
 
         $this->assertEquals('OOOOOOOOOOI',$actual);
     }
 
     public function test_convert_given10Minutes_shouldReturn02_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('10');
+        $actual = $this->getMinutes_per_05('10');
 
         $this->assertEquals('OOOOOOOOOII',$actual);
     }
 
     public function test_convert_given15Minutes_shouldReturn03_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('15');
+        $actual = $this->getMinutes_per_05('15');
 
         $this->assertEquals('OOOOOOOOIII',$actual);
     }
 
     public function test_convert_given20Minutes_shouldReturn04_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('20');
+        $actual = $this->getMinutes_per_05('20');
 
         $this->assertEquals('OOOOOOOIIII',$actual);
     }
 
     public function test_convert_given25Minutes_shouldReturn05_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('25');
+        $actual = $this->getMinutes_per_05('25');
 
         $this->assertEquals('OOOOOOIIIII',$actual);
     }
 
     public function test_convert_given30Minutes_shouldReturn06_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('30');
+        $actual = $this->getMinutes_per_05('30');
 
         $this->assertEquals('OOOOOIIIIII',$actual);
     }
 
     public function test_convert_given35Minutes_shouldReturn07_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('35');
+        $actual = $this->getMinutes_per_05('35');
 
         $this->assertEquals('OOOOIIIIIII',$actual);
     }
 
     public function test_convert_given40Minutes_shouldReturn08_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('40');
+        $actual = $this->getMinutes_per_05('40');
 
         $this->assertEquals('OOOIIIIIIII',$actual);
     }
 
     public function test_convert_given45Minutes_shouldReturn09_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('45');
+        $actual = $this->getMinutes_per_05('45');
 
         $this->assertEquals('OOIIIIIIIII',$actual);
     }
 
     public function test_convert_given50Minutes_shouldReturn10_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('50');
+        $actual = $this->getMinutes_per_05('50');
 
         $this->assertEquals('OIIIIIIIIII',$actual);
     }
 
     public function test_convert_given55Minutes_shouldReturn11_SecondLineLight(){
-        $actual = $this->berlinClock->minutes_per_05('55');
+        $actual = $this->getMinutes_per_05('55');
 
         $this->assertEquals('IIIIIIIIIII',$actual);
     }
 
     public function test_convert_given00Hours_shouldReturn00_ThirdLineLight(){
-        $actual = $this->berlinClock->hours('00');
+        $actual = $this->getHours('00');
 
         $this->assertEquals('OOOO',$actual);
     }
 
     public function test_convert_given01Hours_shouldReturn01_ThirdLineLight(){
-        $actual = $this->berlinClock->hours('01');
+        $actual = $this->getHours('01');
 
         $this->assertEquals('OOOI',$actual);
     }
 
     public function test_convert_given02Hours_shouldReturn02_ThirdLineLight(){
-        $actual = $this->berlinClock->hours('02');
+        $actual = $this->getHours('02');
 
         $this->assertEquals('OOII',$actual);
     }
 
     public function test_convert_given03Hours_shouldReturn03_ThirdLineLight(){
-        $actual = $this->berlinClock->hours('03');
+        $actual = $this->getHours('03');
 
         $this->assertEquals('OIII',$actual);
     }
 
     public function test_convert_given04Hours_shouldReturn04_ThirdLineLight(){
-        $actual = $this->berlinClock->hours('04');
+        $actual = $this->getHours('04');
 
         $this->assertEquals('IIII',$actual);
     }
@@ -210,6 +210,16 @@ class BerlinClockTest extends TestCase
     private function getMinutes(String $string): string
     {
         return $this->berlinClock->minutes($string);
+    }
+
+    private function getMinutes_per_05(String $string): string
+    {
+        return $this->berlinClock->minutes_per_05($string);
+    }
+
+    private function getHours(String $string): string
+    {
+        return $this->berlinClock->hours($string);
     }
 
 }
